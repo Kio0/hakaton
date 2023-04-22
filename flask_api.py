c92abc8969f45b2f802d2e1caea380ad3e190ffa
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2023-04-22 17:02:10 +0300

    Add universal endpoint

--- flask_api.py
+++ flask_api.py
@@ -178,7 +178,7 @@ def service_map_request():
     return jsonify({'services': services})
 
 
-@app.route('/<table>', methods=['GET'])
+@app.route('/table/<table>', methods=['GET'])
 def table_request(table):
     # получаем токен из заголовков
     token = request.headers.get('token')
